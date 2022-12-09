e2107e71c37267551923166f50f82f776e7a7950
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-10 00:36:21 +0900

    feat: 로그인 페이지

---
+++ client/src/views/pages/Login.js
@@ -0,0 +1,14 @@
+import KakaoLogin from 'assets/images/kakao_login_medium_wide.png';
+
+const Login = `
+  <main class='Login__Main'>
+    <div class='Login__Contents'>
+      <h1 class='Login__Title'>Simple Todo List</h1>
+      <button>
+        <img src=${KakaoLogin} alt='kakao_login'  />
+      </button>
+    </div>
+  </main>
+`;
+
+export default Login;
