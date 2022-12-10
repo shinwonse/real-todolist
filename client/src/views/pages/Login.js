4527d2e97e99e42ce3e317a1a2dac7aa29067655
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-10 17:09:21 +0900

    style: font-size 변

--- client/src/views/pages/Login.js
+++ client/src/views/pages/Login.js
@@ -3,8 +3,8 @@ import KakaoLogin from 'assets/images/kakao_login_medium_wide.png';
 const Login = `
   <main class='Login__Main'>
     <div class='Login__Contents'>
-      <h1 class='Login__Title'>Simple Todo List</h1>
-      <button>
+      <h1 class='Login__Title'>Real TodoList</h1>
+      <button class='Login__Button'>
         <img src=${KakaoLogin} alt='kakao_login'  />
       </button>
     </div>
