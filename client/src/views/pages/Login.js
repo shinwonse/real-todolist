955beea2ab905157f66fe34a99d0dc17d9ff597b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-11 22:38:19 +0900

    feat: Login 페이지 컴포넌트화

--- client/src/views/pages/Login.js
+++ client/src/views/pages/Login.js
@@ -1,14 +1,26 @@
 import KakaoLogin from 'assets/images/kakao_login_medium_wide.png';
 
-const Login = `
-  <main class='Login__Main'>
-    <div class='Login__Contents'>
-      <h1 class='Login__Title'>Real TodoList</h1>
-      <button class='Login__Button'>
-        <img src=${KakaoLogin} alt='kakao_login'  />
-      </button>
-    </div>
-  </main>
-`;
+import Component from '../../core/Component';
 
-export default Login;
+class LoginPage extends Component {
+  template() {
+    return `
+      <main class='Login__Main'>
+        <div class='Login__Contents'>
+          <h1 class='Login__Title'>Real TodoList</h1>
+          <button class='Login__Button'>
+            <img src=${KakaoLogin} alt='kakao_login'  />
+          </button>
+        </div>
+      </main>
+    `;
+  }
+
+  setEvent() {
+    this.addEvent('click', '.Login__Button', () => {
+      console.log('click');
+    });
+  }
+}
+
+export default LoginPage;
