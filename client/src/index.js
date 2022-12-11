30e6998db9ce1a6dbb85f55144d9e8149f261c00
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-11 23:42:39 +0900

    feat: index.js 수정

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,6 @@
 import 'assets/styles/base.scss';
-import Login from 'pages/Login';
-import TodoList from 'pages/TodoList';
 
-class App {
-  constructor() {
-    const $app = document.querySelector('#app');
-    new TodoList($app);
-  }
-}
+import Router from './Router';
 
-new App();
+const router = new Router();
+router.init();
