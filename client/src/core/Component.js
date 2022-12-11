c2fcdbc736ddf45cde603fd5d4817aff0b817642
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-11 22:38:05 +0900

    feat: Component 클래스 생성

---
+++ client/src/core/Component.js
@@ -0,0 +1,35 @@
+class Component {
+  $target;
+  $state;
+  constructor($target) {
+    this.$target = $target;
+    this.setup();
+    this.render();
+  }
+  setup() {}
+  template() {
+    return '';
+  }
+  render() {
+    this.$target.innerHTML = this.template();
+    this.setEvent();
+  }
+  setEvent() {}
+  setState(newState) {
+    this.$state = { ...this.$state, ...newState };
+    this.render();
+  }
+  addEvent(eventType, selector, callback) {
+    const children = [...this.$target.querySelectorAll(selector)];
+    // selector에 명시한 것 보다 더 하위 요소가 선택되는 경우가 있을 땐
+    // closest를 이용하여 처리한다.
+    const isTarget = (target) =>
+      children.includes(target) || target.closest(selector);
+    this.$target.addEventListener(eventType, (event) => {
+      if (!isTarget(event.target)) return false;
+      callback(event);
+    });
+  }
+}
+
+export default Component;
