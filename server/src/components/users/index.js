fd7a37fa564ed9a37d71a384f71a38981a832f7a
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-15 16:36:08 +0900

    feat: testing

--- server/src/components/users/index.js
+++ server/src/components/users/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('hello users!!!!!!');
+  res.send('plz');
 });
 
 module.exports = router;
