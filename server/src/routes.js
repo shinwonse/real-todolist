2efdafc1d52b191f437d2dcf3a911314acb4a1cb
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-15 14:39:49 +0900

    feat: update routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,9 +4,6 @@ const router = express.Router();
 const oauth = require('./components/oauth');
 const users = require('./components/users');
 
-router.use((req, res) => {
-  res.send('Hello World!');
-});
 router.use('/oauth', oauth);
 router.use('/users', users);
 
