c934c2ebcb3399956cad0c1f77813fbf4aeab83a
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-16 17:03:53 +0900

    feat: redis 연결

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,52 @@
+const connectRedis = require('connect-redis');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const express = require('express');
+const session = require('express-session');
 const mongoose = require('mongoose');
+const redis = require('redis');
 
 const { MONGO_ID, MONGO_PW, MONGO_URI } = require('./config/mongo');
+const {
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_USERNAME,
+  REDIS_PASSWORD,
+} = require('./config/redis');
 const routes = require('./routes');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
+
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient({
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+  username: REDIS_USERNAME,
+  password: REDIS_PASSWORD,
+  legacyMode: true,
+});
+
+const sessionOption = {
+  resave: false,
+  saveUninitialized: true,
+  secret: 'secret',
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  name: 'auth',
+  store: new RedisStore({ client: redisClient, prefix: 'auth' }),
+};
+
+app.use(session(sessionOption));
+
+(async () => {
+  await redisClient.connect();
+})();
+app.use(session(sessionOption));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/api', routes);
 
