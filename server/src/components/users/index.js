500be9e43150002525b83caf5b8f81f9165fdfec
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-18 19:14:16 +0900

    feat: user api 작성중

--- server/src/components/users/index.js
+++ server/src/components/users/index.js
@@ -1,8 +1,55 @@
 const express = require('express');
+
+const { Todo } = require('../toDos/model');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('plz 제발');
+router.get('/:nickname', (req, res) => {
+  const { nickname } = req.params;
+  res.send(nickname);
+});
+
+router.get('/:nickname/todos', (req, res) => {
+  Todo.find()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+});
+
+router.post('/:nickname/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save((err, todo) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(todo);
+  });
+});
+
+router.delete('/:nickname/todos/:todoID', (req, res) => {
+  Todo.deleteOne({ _id: req.params.todoID })
+    .then(() => res.json({ message: 'Todo Deleted' }))
+    .catch((err) => res.send(err));
+});
+
+router.put('/:nickname/todos/:todoID', (req, res) => {
+  Todo.findOneAndUpdate(
+    { _id: req.params.todoID },
+    { $set: { text: req.body.text } },
+    { new: true },
+    (err, Todo) => {
+      if (err) {
+        res.send(err);
+      } else res.json(Todo);
+    }
+  );
 });
 
 module.exports = router;
