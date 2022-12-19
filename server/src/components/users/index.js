0a7b34abcd3702680960d21801e773085593d541
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-20 00:51:47 +0900

    feat: 유저 조회 api 수정중

--- server/src/components/users/index.js
+++ server/src/components/users/index.js
@@ -1,23 +1,29 @@
 const express = require('express');
 
 const { Todo } = require('../toDos/model');
+
+const { User } = require('./model');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  if (req.session.loggedIn) {
+    return res.send(req.session);
+  }
+  res.status(401).send('로그인이 필요합니다.');
+});
+
 router.get('/:nickname', (req, res) => {
-  const { nickname } = req.params;
-  res.send(nickname);
+  if (req.cookies.auth) {
+    User.findOne({ nickname: req.params.nickname }, (err, user) => {
+      if (err) return res.status(500).json({ error: err });
+      if (!user) return res.status(404).json({ error: 'User not found' });
+      return res.status(200).json(user);
+    });
+  } else res.status(404).send('로그인이 필요합니다.');
 });
 
 router.get('/:nickname/todos', (req, res) => {
-  Todo.find()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message,
-      });
-    });
+  res.end();
 });
 
 router.post('/:nickname/todos', (req, res) => {
