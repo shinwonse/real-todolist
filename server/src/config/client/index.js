e230cbd6dc39b76474a4e47ef50cf69370acfa13
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-20 00:52:16 +0900

    feat: CLIENT_BASE_URI 추가

--- server/src/config/client/index.js
+++ server/src/config/client/index.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const CLIENT_BASE_URI = process.env.CLIENT_BASE_URI;
 const CLIENT_REDIRECT_URI = process.env.CLIENT_REDIRECT_URI;
 
-module.exports = { CLIENT_REDIRECT_URI };
+module.exports = { CLIENT_BASE_URI, CLIENT_REDIRECT_URI };
