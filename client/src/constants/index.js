61c03a3b3b4a63d7220755e69b4ca206420ff5fe
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-20 00:51:34 +0900

    feat: SERVER_BASE_URI 추가

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,1 +1,2 @@
+export const SERVER_BASE_URI = process.env.SERVER_BASE_URI;
 export const KAKAO_AUTHORIZATION_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code`;
