53522fe0c586dafbeb1151656078099922dca025
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-28 00:03:09 +0900

    feat($client-core): core Component Vue lifecycle 참고하여 수정

--- client/src/core/Component.js
+++ client/src/core/Component.js
@@ -1,27 +1,54 @@
+import { bind } from 'utils';
+
 class Component {
   $target;
-  // $props;
-  $state;
-  constructor($target) {
+  props;
+  state;
+  constructor($target, props) {
+    this.init($target, props);
+  }
+
+  async init($target, props) {
+    this.beforeCreate();
+
     this.$target = $target;
-    // this.$props = $props;
+    this.props = props;
+
+    await this.created();
+    this.state = this.initState();
+    await this.beforeMount();
     this.render();
+    await this.mounted();
   }
-  setup() {}
+
+  /* lifecycles */
+  beforeCreate() {}
+  created() {}
+  beforeMount() {}
   mounted() {}
-  template() {
-    return '';
-  }
-  async render() {
-    await this.setup();
-    this.$target.innerHTML = this.template();
-    this.setEvent();
-  }
+  beforeUpdate() {}
+  updated() {}
+  /* lifecycles */
+
+  /* abstract methods */
+  initState() {}
+  template() {}
   setEvent() {}
-  setState(newState) {
-    this.$state = { ...this.$state, ...newState };
-    this.render();
+  /* abstract methods */
+
+  render() {
+    const template = this.template();
+    this.$target.innerHTML = bind.apply(this, [template]);
   }
+
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+    this.beforeUpdate();
+    this.render();
+    this.setEvent();
+    this.updated();
+  }
+
   addEvent(eventType, selector, callback) {
     const children = [...this.$target.querySelectorAll(selector)];
     const isTarget = (target) =>
