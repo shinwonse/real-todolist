92aa6d087527c59bfe33ef146aea4328040a2b63
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-28 22:18:15 +0900

    feat($client-index): 경로 alias 수 및 api 호출 코드 분리

--- client/src/views/components/MoreOptionModal.js
+++ client/src/views/components/MoreOptionModal.js
@@ -1,38 +1,7 @@
-import DeleteIcon from 'assets/icons/icon-delete.svg';
-import EditIcon from 'assets/icons/icon-edit.svg';
-import SubmitIcon from 'assets/icons/icon-submit.svg';
-import axios from 'axios';
-
-import Component from '../../core/Component';
-
-const closeModal = () => {
-  const modal = document.querySelector('.Modal');
-  if (modal) {
-    modal.remove();
-  }
-};
-
-const startEdit = () => {
-  const contentDiv = document.querySelector('.Modal__Content');
-  contentDiv.innerHTML = `
-    <form class='Modal__Form'>
-      <input type='text' class='Modal__Input' placeholder='수정할 할 일을 입력하세요.'/>
-      <button class='Modal__Button' type='submit' class='Modal__Submit'>
-        <img class='Modal__Icon--edit' alt='submit' src=${SubmitIcon} />
-      </button>
-    </form>
-  `;
-};
-
-const deleteTodo = async (id) => {
-  await axios.delete(
-    `http://localhost:3000/api/todos/63a838365e25479c2ef410b5`,
-    {
-      withCredentials: true,
-      headers: { 'Content-Type': 'application/json' },
-    }
-  );
-};
+import DeleteIcon from '@/assets/icons/icon-delete.svg';
+import EditIcon from '@/assets/icons/icon-edit.svg';
+import SubmitIcon from '@/assets/icons/icon-submit.svg';
+import Component from '@/core/Component';
 
 class MoreOptionModal extends Component {
   template() {
@@ -53,10 +22,29 @@ class MoreOptionModal extends Component {
     `;
   }
 
+  startEdit = () => {
+    const contentDiv = document.querySelector('.Modal__Content');
+    contentDiv.innerHTML = `
+    <form class='Modal__Form'>
+      <input type='text' class='Modal__Input' placeholder='수정할 할 일을 입력하세요.'/>
+      <button class='Modal__Button' type='submit' class='Modal__Submit'>
+        <img class='Modal__Icon--edit' alt='submit' src=${SubmitIcon} />
+      </button>
+    </form>
+  `;
+  };
+
+  closeModal = () => {
+    const modal = document.querySelector('.Modal');
+    if (modal) {
+      modal.remove();
+    }
+  };
+
   setEvent() {
-    this.addEvent('click', '.Modal__Overlay', closeModal);
-    this.addEvent('click', '#edit', startEdit);
-    this.addEvent('click', '#delete', deleteTodo);
+    this.addEvent('click', '.Modal__Overlay', this.closeModal);
+    this.addEvent('click', '#edit', this.startEdit);
+    // this.addEvent('click', '#delete', this.deleteTodo);
   }
 }
 
