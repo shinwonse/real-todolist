7a938d90f1c55c4ff9b343759395dda5d64d7541
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-12-30 20:49:29 +0900

    feat($client-Router): Router 업데이트

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,5 +1,4 @@
-import axios from 'axios';
-
+import { fetchUser } from '@/api/todoList';
 import Login from '@/views/pages/Login';
 import TodoList from '@/views/pages/TodoList';
 
@@ -26,37 +25,21 @@ class Router {
   }
 
   init() {
-    const { path, auth, component } = routes.find(
+    const { auth, component } = routes.find(
       (route) => route.path === location.pathname
     );
     if (auth) {
-      axios
-        .get(`http://localhost:3000/api/users`, { withCredentials: true })
-        .catch(() => {
-          Router.push('/login');
-        });
-    }
-    if (path === '/login') {
+      fetchUser();
       return new component(this.$app);
     }
-    if (path === '/') {
-      return new component(this.$app);
-    }
+    return new component(this.$app);
   }
 
   static push(destination) {
     const $app = document.querySelector('#app');
-    const { path, auth, component } = routes.find(
+    const { path, component } = routes.find(
       (route) => route.path === destination
     );
-    if (auth) {
-      axios
-        .get(`http://localhost:3000/api/users`, { withCredentials: true })
-        .catch(() => {
-          Router.push('/login');
-        });
-      return new component($app);
-    }
     history.pushState(null, null, path);
     return new component($app);
   }
