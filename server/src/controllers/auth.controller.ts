0fa83a0adc071ff3c185c455540948dbab6b12c7
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-08 19:51:02 +0900

    feat($server): redirect uri

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { NextFunction, Request, Response } from 'express';
 import qs from 'qs';
 import {
-    CLIENT_REDIRECT_URI_DEV,
-    CLIENT_REDIRECT_URI_PRODUCTION,
-    KAKAO_CLIENT_ID,
-    KAKAO_REDIRECT_URI
+  CLIENT_REDIRECT_URI_DEV,
+  CLIENT_REDIRECT_URI_PRODUCTION,
+  KAKAO_CLIENT_ID,
+  KAKAO_REDIRECT_URI,
 } from '../config/env';
 import UsersService from '../services/users.service';
 import { User } from '../entities/user.entity';
@@ -15,16 +15,19 @@ import { plainToInstance } from 'class-transformer';
 
 let redirectURI;
 if (process.env.NODE_ENV == 'production') {
-    redirectURI = CLIENT_REDIRECT_URI_PRODUCTION
+  redirectURI = CLIENT_REDIRECT_URI_PRODUCTION;
 } else if (process.env.NODE_ENV == 'development') {
-    redirectURI = CLIENT_REDIRECT_URI_DEV
+  redirectURI = CLIENT_REDIRECT_URI_DEV;
 }
 
 class AuthController {
   public usersService = new UsersService();
 
-
-  public goRedirectURL = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public goRedirectURL = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
     try {
       const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
@@ -34,8 +37,11 @@ class AuthController {
     }
   };
 
-  public getKakaoRedirect = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-
+  public getKakaoRedirect = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
     // 인증 코드를 먼저 받아오는 부분
     let kakaoTokenRequest;
     try {
@@ -83,7 +89,7 @@ class AuthController {
 
       // 가입된 유저가 있는지 확인하고 없으면 생성
       if (isEmpty(loginUser)) {
-        let userData = plainToInstance(UserDto, {
+        const userData = plainToInstance(UserDto, {
           nickname: kakaoNickname,
           is_kakao: true,
           kakao_id: kakaoId,
@@ -94,29 +100,37 @@ class AuthController {
       req.session.isLogin = true;
       req.session.loginedUser = loginUser;
       req.session.save();
-      res.redirect(redirectURI);
-    }
-    catch (error) {
+      console.log(redirectURI);
+      res.redirect('http://localhost:8080');
+    } catch (error) {
       next(error);
     }
   };
 
-  public logout = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    if(req.session.isLogin){
-      req.session.destroy((err)=>{
-        if(err) throw err;
+  public logout = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    if (req.session.isLogin) {
+      req.session.destroy((err) => {
+        if (err) throw err;
         res.redirect(redirectURI);
-      })
+      });
     } else {
       res.redirect(redirectURI);
     }
   };
 
-  public islogin = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    if(req.session.isLogin){
-      res.status(200).json({data:"logined", user:req.session.loginedUser})
+  public islogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    if (req.session.isLogin) {
+      res.status(200).json({ data: 'logined', user: req.session.loginedUser });
     } else {
-      res.status(401).json({data:"Not logined"})
+      res.status(401).json({ data: 'Not logined' });
     }
   };
 }
