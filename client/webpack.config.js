f7e8c300c108b2c5e257b839aa4cdd31f8a87cc9
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-09 01:06:13 +0900

    feat($client): dotenv-webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const Dotenv = require('dotenv-webpack');
-dotenv.config();
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
