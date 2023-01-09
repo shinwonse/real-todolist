7d897823809e28635e5959db8f564229d6fface4
hoosa <janghoosa@gmail.com>
2023-01-09 17:57:44 +0900

    feat($server): authmiddleware 적용

--- server/src/routes/todos.routes.ts
+++ server/src/routes/todos.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import TodosController from '../controllers/todos.controller';
 import { TodoDto } from '../dtos/todo.dto';
 import validationMiddleware from '../middlewares/validation.middleware';
+import authMiddleware from '../middlewares/auth.middleware';
 
 class TodosRoutes {
   public path = '/todos';
@@ -14,14 +15,20 @@ class TodosRoutes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}/all`, this.todosController.getAllTodos);
-    this.router.get(`${this.path}`, this.todosController.getTodosByUserId);
+    this.router.get(
+      `${this.path}`,
+      authMiddleware,
+      this.todosController.getTodosByUserId,
+    );
     this.router.post(
       `${this.path}`,
+      authMiddleware,
       validationMiddleware(TodoDto, 'body'),
       this.todosController.createTodo,
     );
     this.router.put(
       `${this.path}/:id(\\d+)`,
+      authMiddleware,
       validationMiddleware(TodoDto, 'body'),
       this.todosController.updateTodo,
     );
