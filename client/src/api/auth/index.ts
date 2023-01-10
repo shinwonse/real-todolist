3856634953a4c9c24e49ec43551f2304d2ac1439
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-10 20:26:23 +0900

    refactor: kakaoLogin function 리팩토링

--- client/src/api/auth/index.ts
+++ client/src/api/auth/index.ts
@@ -2,9 +2,7 @@ import axios from '@/api/axios';
 import { KAKAO_AUTHORIZATION_URL } from '@/constants';
 import Router from '@/Router';
 
-export const kakaoLogin = () => {
-  location.href = KAKAO_AUTHORIZATION_URL;
-};
+export const kakaoLogin = () => (location.href = KAKAO_AUTHORIZATION_URL);
 
 export const logout = () => {
   axios.get('/oauth/logout').then(() => {
