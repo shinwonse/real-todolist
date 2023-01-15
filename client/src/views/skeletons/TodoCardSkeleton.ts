3f61ca4e3041606bee8e4264e8db029b81a7bea2
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-15 23:56:38 +0900

    refactor: skeleton 수정

--- client/src/views/skeletons/TodoCardSkeleton.ts
+++ client/src/views/skeletons/TodoCardSkeleton.ts
@@ -1,22 +1,11 @@
 import CardSkeletonStyle from '@/assets/styles/scss/cardSkeleton.module.scss';
-import Component from '@/core/Component';
 
-class TodoCardSkeleton extends Component {
-  initState() {
-    return {
-      toDos: Array.from({ length: 5 }, () => ({})),
-    };
-  }
+const mockData = Array.from({ length: 5 }, () => ({}));
 
-  template() {
-    return `
-      <ul class=${CardSkeletonStyle.ul}>
-        ${this.state.toDos
-          .map(() => `<li class=${CardSkeletonStyle.li}></li>`)
-          .join('')}
-      </ul>
-    `;
-  }
-}
+const TodoCardSkeleton = `
+  <ul class=${CardSkeletonStyle.ul}>
+    ${mockData.map(() => `<li class=${CardSkeletonStyle.li}></li>`).join('')}
+  </ul>
+`;
 
 export default TodoCardSkeleton;
--- client/src/views/skeletons/UsernameSkeleton.ts
+++ client/src/views/skeletons/UsernameSkeleton.ts
@@ -1,14 +1,9 @@
 import UsernameSkeletonStyle from '@/assets/styles/scss/userNameSkeleton.module.scss';
-import Component from '@/core/Component';
 
-class UsernameSkeleton extends Component {
-  template() {
-    return `
-      <div class=${UsernameSkeletonStyle.wrapper}>
-        <h1>skeleton title</h1>
-      </div>
-    `;
-  }
-}
+const UsernameSkeleton = `
+  <div class=${UsernameSkeletonStyle.wrapper}>
+    <h1>skeleton title</h1>
+  </div>
+`;
 
 export default UsernameSkeleton;
