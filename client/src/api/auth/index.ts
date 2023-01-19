47cb1e5a26b12d9b68c423085e35133597ca6035
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-19 23:38:31 +0900

    bug: production env undefined 오류

--- client/src/api/auth/index.ts
+++ client/src/api/auth/index.ts
@@ -3,8 +3,8 @@ import { KAKAO_CLIENT_ID, KAKAO_REDIRECT_URI } from '@/constants';
 import Router from '@/Router';
 import { deleteToken } from '@/utils';
 
-const KAKAO_AUTHORIZATION_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
-// const KAKAO_AUTHORIZATION_URL = `https://kauth.kakao.com/oauth/authorize?client_id=e6f625075c7051ea84103a3d42dc9a36&redirect_uri=http://localhost:3000/oauth/kakao/redirect&response_type=code`;
+// const KAKAO_AUTHORIZATION_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+const KAKAO_AUTHORIZATION_URL = `https://kauth.kakao.com/oauth/authorize?client_id=e6f625075c7051ea84103a3d42dc9a36&redirect_uri=https://real-todolist.herokuapp.com/oauth/kakao/redirect&response_type=code`;
 console.log(KAKAO_AUTHORIZATION_URL);
 
 export const kakaoLogin = () => (location.href = KAKAO_AUTHORIZATION_URL);
