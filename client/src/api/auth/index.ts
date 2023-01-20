df99d3467a13d1b380acd58f81e70f3e19f55204
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-20 19:55:56 +0900

    feat: getTodoById 추가

--- client/src/api/auth/index.ts
+++ client/src/api/auth/index.ts
@@ -3,9 +3,7 @@ import { KAKAO_CLIENT_ID, KAKAO_REDIRECT_URI } from '@/constants';
 import Router from '@/Router';
 import { deleteToken } from '@/utils';
 
-console.log(process.env.KAKAO_CLIENT_ID);
 const KAKAO_AUTHORIZATION_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
-console.log(KAKAO_AUTHORIZATION_URL);
 
 export const kakaoLogin = () => (location.href = KAKAO_AUTHORIZATION_URL);
 
